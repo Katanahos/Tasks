b8ac4261ecc501e0df8fa55de71fcdad58473aa3
Katanahos <katanahos@gmail.com>
2020-08-31 23:26:43 +0300

    testing

---
+++ src/Task1.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Task1 {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        double a = scanner.nextDouble();
+        double b = scanner.nextDouble();
+        double c = scanner.nextDouble();
+
+        double z;
+
+        z = ((a-3)*b/2)+c;
+        System.out.println(z);
+
+        // write your code here
+    }
+}
