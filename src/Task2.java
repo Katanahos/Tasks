c85c47da62a4bb7749bc919349f23111b6805a03
Katanahos <katanahos@gmail.com>
2020-08-31 23:41:37 +0300

    Task2

--- src/Task2.java
+++ src/Task2.java
@@ -1,2 +1,15 @@
+import java.util.Scanner;
+
+
 public class Task2 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double a = scanner.nextDouble();
+        double b = scanner.nextDouble();
+        double c = scanner.nextDouble();
+
+        double answer = ((b+Math.sqrt((b*b+(4*a*c))))/(2*a))-((a*a*a*c)+1/(b*b));
+        System.out.println(answer);
+    }
 }
