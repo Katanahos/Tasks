7c772997fb5c7f0084aaf89e12d98badd7bc7c27
Katanahos <katanahos@gmail.com>
2020-09-01 00:09:19 +0300

    Task3

---
+++ src/Task4.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Task4 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double x = scanner.nextDouble();
+
+        Integer a = (int) x;
+        double drob = x-a;
+        double answer = drob*1000+((double)a/1000);
+        System.out.format("%.3f%n", answer);
+    }
+}
+
